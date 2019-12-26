404d8d011a0f5c1785d08063e406f29eb539328e
unknown <you@example.com>
2019-12-26 16:14:42 +0200

    add converter

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -1,14 +1,18 @@
 import styles from './Article.module.css'
 import React from "react";
+import Converter from "./Converter";
 
 
 class Article extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            USD: {},
-            1: {},
-            2: {}
+            rate: {
+                USD: {},
+                EUR: {},
+                RUR: {}
+
+            }
         }
         this.getRate()
     }
@@ -19,40 +23,48 @@ class Article extends React.Component {
                 return data.json()
             })
             .then(data => {
-                this.setState({
-                    base_ccyUAH: data[0].base_ccy,
-                    ccyUSD: data[0].ccy,
-                    buyUSD: data[0].buy,
-                    saleUSD: data[0].sale,
-
-                    base_ccyUAH: data[1].base_ccy,
-                    ccyEUR: data[1].ccy,
-                    buyEUR: data[1].buy,
-                    saleEUR: data[1].sale,
-
-                    base_ccyUAH: data[2].base_ccy,
-                    ccyRUR: data[2].ccy,
-                    buyRUR: data[2].buy,
-                    saleRUR: data[2].sale,
-
-                })
+                this.setState(
+                    {
+                        rate: {
+                            USD: data[0],
+                            EUR: data[1],
+                            RUR: data[2],
+                        }
+                    })
                 console.log(data);
             })
     }
-
 
     render() {
 
         return (
             <div className={styles.Article}>
                 <span
-                    className={styles.item1}> Курс валют на {this.state.base_ccyUAH} {this.state.ccyEUR}  </span>
-                <span className={styles.item2}>USD </span>
-                <span className={styles.item3}>USD </span>
-                <span className={styles.item4}>USD </span>
-                <span className={styles.item5}>Калькулятор обмена </span>
+                    className={styles.item1}> Курс валют на ../../....г.  </span>
+
+                <span className={styles.item2}>Курс к гривне </span>
+                <span className={styles.item3}>Продажа </span>
+                <span className={styles.item4}>Покупка </span>
                 <span
-                    className={styles.item6}>Я хочу: Купить/продать, Результат </span>
+                    className={styles.item5}>{this.state.rate.USD.ccy}</span>
+                <span
+                    className={styles.item6}>{this.state.rate.USD.sale}</span>
+                <span
+                    className={styles.item7}>{this.state.rate.USD.buy}</span>
+                <span
+                    className={styles.item8}>{this.state.rate.EUR.ccy} </span>
+                <span
+                    className={styles.item9}>{this.state.rate.EUR.sale} </span>
+                <span
+                    className={styles.item10}>{this.state.rate.EUR.buy} </span>
+                <span
+                    className={styles.item11}>{this.state.rate.RUR.ccy} </span>
+                <span
+                    className={styles.item12}>{this.state.rate.RUR.sale} </span>
+                <span
+                    className={styles.item13}>{this.state.rate.RUR.buy} </span>
+
+                <Converter rate={this.state.rate}/>
             </div>
 
         )
